99c2d7925a43d0b829a28cb48b416bb5a19d7eda
tr0n_xD <tr0n_xD@protonmail.com>
2023-02-17 22:51:57 +0100

    first commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Welcome to Kaizen DAO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+            改善道
+        </p>
+        <p>
+            カイゼン道へようこそ
+        </p>
       </header>
     </div>
   );
