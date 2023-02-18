7b99b51b65cd29a1789722a67e9574250dfe1549
tr0n_xD <tr0n_xD@protonmail.com>
2023-02-18 16:54:04 +0100

    fix build

--- src/Daos.tsx
+++ src/Daos.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "react-i18next";
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Daos() {
   const {t} = useTranslation();
