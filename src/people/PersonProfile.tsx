065e74e7063d9d849333ef14fcc1f40867f8368c
tr0n_xD <tr0n_xD@protonmail.com>
2023-03-15 14:39:10 +0100

    last change for now.

--- src/people/PersonProfile.tsx
+++ src/people/PersonProfile.tsx
@@ -1,7 +1,9 @@
 import { Person } from "../dao/DaoTypes";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export function PersonProfile(props: {person: Person}) {
+    const {t} = useTranslation();
     const x = props.person;
 
     function getCompanyImage(company: string) {
@@ -10,9 +12,9 @@ export function PersonProfile(props: {person: Person}) {
 
     return (
         <div className='flexColumn justifyStart gap10 bg-yellow' style={{width: '300px', color: 'black', borderRadius: '10px', padding: '15px 10px 15px 10px', fontSize: '15px', textAlign: 'left'}}>
-            <div style={{fontWeight: 'bold'}}>Name: {x.name}</div>
-            <div>Position: {x.position}</div>
-            <div>Company: {x.company}</div>
+            <div style={{fontWeight: 'bold'}}>{t('person.name')} {x.name}</div>
+            <div>{t('person.position')} {x.position}</div>
+            <div>{t('person.company')} {x.company}</div>
             <div className='flexRow'>
                 <img alt='' height='50px' src={getCompanyImage(x.company)} style={{borderRadius: '25px'}}/>
                 <img alt='' height='50px' src={x.avatar} style={{borderRadius: '25px'}}/>
