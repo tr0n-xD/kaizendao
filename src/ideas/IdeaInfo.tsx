8ad65f0c43c6dc50d241b1eaa136a3224863244f
tr0n_xD <tr0n_xD@protonmail.com>
2023-03-19 15:00:06 +0100

    fix show goal in ideainfo

--- src/ideas/IdeaInfo.tsx
+++ src/ideas/IdeaInfo.tsx
@@ -1,8 +1,10 @@
 import { Idea } from "../dao/DaoTypes";
-import React from "react";
+import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
+import { KaizenDaoContext } from "../dao/DaoContext";
 
 export function IdeaInfo(props: {idea: Idea, selectOkay: any}) {
+    const kaizen = useContext(KaizenDaoContext);
     const {t} = useTranslation();
     const x = props.idea;
     return (
@@ -12,7 +14,7 @@ export function IdeaInfo(props: {idea: Idea, selectOkay: any}) {
             <div>{t('idea.details')}</div>
             <img alt='' height='75px' src='/icon-ideas.png'/>
             <div style={{fontStyle: 'italic'}}>{t('idea.proposed_by')} {x.author}</div>
-            <div style={{fontStyle: 'italic'}}>{t('idea.solves_goal')} #1 (Reduce costs)</div>
+            <div style={{fontStyle: 'italic'}}>{t('idea.solves_goal')} #{x.goal} ({kaizen.goals[x.goal-1].desc})</div>
             <div className='flexRow gap5'>
                 <div style={{padding: '3px 5px 0px 5px'}}><img alt='' height='20px' src='/token-32.png'/></div>
                 <div>{x.points} / {x.totalPoints} tokens</div>
